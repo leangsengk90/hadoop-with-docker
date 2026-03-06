21e8ea94e59c02ec465fa5cae4a1366b0121eab5
leangsengk90 <leangsengk90@gmail.com>
2026-03-07 00:07:40 +0700

    Add description

--- config/hadoop-env.sh
+++ config/hadoop-env.sh
@@ -1,10 +1,11 @@
+ # Java runtime for Hadoop processes.
 export JAVA_HOME=/usr/local/java
 
-# Add these for HDFS
+# Run HDFS daemons as root inside the container.
 export HDFS_NAMENODE_USER=root
 export HDFS_DATANODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
 
-# Add these for YARN
+# Run YARN daemons as root inside the container.
 export YARN_RESOURCEMANAGER_USER=root
 export YARN_NODEMANAGER_USER=root
