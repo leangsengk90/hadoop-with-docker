7f15abae637510964a4f3dc284e19ae7b5b6809c
leangsengk90 <leangsengk90@gmail.com>
2026-03-07 11:08:28 +0700

    Update save.sh to output part-00000 and modify visualize.sh to install python3-pip with -y flag

--- config/superstore/save.sh
+++ config/superstore/save.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-hdfs dfs -cat /data/superstore/output/part-00000 > part-00000
+hdfs dfs -cat /data/superstore/output/part-00000
+hdfs dfs -get -f /data/superstore/output/part-00000 .
--- config/superstore/visualize.sh
+++ config/superstore/visualize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 --version
+# source venv/bin/activate
 pip3 install matplotlib
+# python3 -m pip install matplotlib
 
 python3 visualize.py
