2f031a3e072644d2f209a6a5c5b9694ce8b2aec4
leangsengk90 <leangsengk90@gmail.com>
2026-04-25 23:18:26 +0700

    Refactor code structure for improved readability and maintainability

--- config/assessment-2/installlib.sh
+++ config/assessment-2/installlib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker exec namenode pip install matplotlib pandas 
-docker exec datanode1 pip install matplotlib pandas 
-docker exec datanode2 pip install matplotlib pandas
+docker exec namenode pip install matplotlib pandas seaborn
+docker exec datanode1 pip install matplotlib pandas seaborn
+docker exec datanode2 pip install matplotlib pandas seaborn
